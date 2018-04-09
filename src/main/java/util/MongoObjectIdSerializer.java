6501419ef83b2f492924739223c9647c96272749
Stevan Jokic <stevan.jokic@gmail.com>
2018-04-09 16:37:36 +0200

    pozweb init

---
+++ src/main/java/util/MongoObjectIdSerializer.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.lang.reflect.Type;
+
+import org.bson.types.ObjectId;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+public class MongoObjectIdSerializer implements JsonSerializer<ObjectId>{
+
+	public JsonElement serialize(ObjectId idObj, Type type, JsonSerializationContext context) {
+		System.out.println(idObj + " " + type + " " +  context);
+//		JsonObject objId = new JsonObject();
+		JsonPrimitive objId = new JsonPrimitive(idObj.toString());
+//		objId.addProperty("_id", idObj.toString());
+		return objId;
+	}
+
+	
+
+}
